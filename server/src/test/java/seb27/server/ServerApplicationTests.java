585fdd3ce22678f3ea21e92be34b97cbee561c1b
klouxia <116135893+klouxia@users.noreply.github.com>
2023-02-17 09:56:49 +0900

    ADD basics
    프로젝트 초기 설정

---
+++ server/src/test/java/seb27/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package seb27.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
