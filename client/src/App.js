81e67a565f28f46ace197bbe38c9951271c89312
es39 <esj10025@gmail.com>
2023-02-17 09:57:16 +0900

    feat: initial setting

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          코에능 화이팅!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
