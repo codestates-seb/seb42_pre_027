80b1097969f50e90916baf11af5a8a76f62f3519
es39 <esj10025@gmail.com>
2023-02-19 23:13:57 +0900

    design: SideBar.js focus apply

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -23,12 +23,7 @@ const Side = styled.div`
     margin: 10px 10px 15px 5px;
     color: #9a9fa5;
   }
-  a {
-    cursor: pointer;
-    text-decoration: none;
-    /* 글씨색 수정 */
-    color: black;
-  }
+
   .nav-links {
     li:not(:first-child) {
       margin-bottom: 15px;
@@ -40,13 +35,30 @@ const Side = styled.div`
     font-size: 0.85em;
     margin-top: 7.5px;
     margin-bottom: 7.5px;
-    &:focus {
-      background-color: gray;
-    }
   }
   #home {
     font-size: 0.85em;
-    margin: 10px 10px 25px 5px;
+  }
+  .content {
+    display: flex;
+    width: 161px;
+    height: 35px;
+    margin-left: 3px;
+    > a {
+      display: flex;
+      align-items: center;
+      cursor: pointer;
+      text-decoration: none;
+      /* 글씨색 수정 */
+      color: black;
+      width: 100%;
+      height: 100%;
+      &:focus {
+        background-color: #f1f2f3;
+        border-right: 3px solid #f48023;
+        font-weight: 700;
+      }
+    }
   }
 `;
 
@@ -56,12 +68,12 @@ const SideBar = () => {
       <Header />
       <Side>
         <ol className="nav-links">
-          <li id="home">
+          <li id="home" className="content">
             <Link to="/main">Home</Link>
           </li>
           <ol className="nav-links">
             <li className="nav-menu">PUBLIC</li>
-            <li className="">
+            <li className="content">
               <Link to="/main">
                 <span>Question</span>
               </Link>
@@ -98,9 +110,9 @@ export default SideBar;
 /* TODO:
 1. 사이드바 스타일 컴포넌트 적용
 1-1. 기본 구성 갖추기 *
-1-2. hover 추가 
-1-3. 선택됐을 시 bold (font-weight: 700)
-1-4. 선택됐을 시 배경 회색 및 주황 띠
+1-2. focus 추가 *
+1-3. 선택됐을 시 bold (font-weight: 700) *
+1-4. 선택됐을 시 배경 회색 및 주황 띠 *
 1-5. 아이콘 추가
-1-6. span 및 Home 글씨색 변경
+1-6. span 및 Home 글씨색 변경 *
 */
