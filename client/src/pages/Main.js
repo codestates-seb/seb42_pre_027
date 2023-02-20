4e5eb937e92989eb38a3f87bad53bc4445125acb
es39 <esj10025@gmail.com>
2023-02-20 22:13:52 +0900

    feat: Main.js button add and Link add

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,7 +1,30 @@
+import styled from 'styled-components';
+import Aside from '../components/Aside';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import SideBar from '../components/SideBar';
+
+import { Link } from 'react-router-dom';
+
+const MainPage = styled.main`
+  display: flex;
+  > button {
+    width: 500px;
+  }
+`;
+
 const Main = () => {
   return (
     <>
-      <div>im main login</div>
+      <Header />
+      <SideBar />
+      <MainPage>
+        <button>
+          <Link to="/createquestion">Ask Question</Link>
+        </button>
+        <Aside />
+      </MainPage>
+      <Footer />
     </>
   );
 };
