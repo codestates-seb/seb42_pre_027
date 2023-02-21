7af7cb5b189a0bbb6a64939546960c2153f0b71d
es39 <esj10025@gmail.com>
2023-02-21 21:19:35 +0900

    design: title, content input css modify

--- client/src/pages/CreateQuestion.js
+++ client/src/pages/CreateQuestion.js
@@ -7,6 +7,7 @@ import Header from '../components/Header';
 const CreateBox = styled.div`
   width: 100%;
   height: 100%;
+  background-color: #f8f9f9;
 `;
 
 const Content = styled.main`
@@ -15,63 +16,108 @@ const Content = styled.main`
   margin: 0 auto;
   height: 100vh;
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: column;
   position: static;
+  padding: 0px 3% 24px;
   /* transform: translate(-50%, -50%);
   top: 50%;
   left: 50%; */
+  .top {
+    display: flex;
+    background-image: url(https://cdn.sstatic.net/Img/ask/background.svg?v=2e9a8205b368);
+    background-position: right center;
+    background-repeat: no-repeat;
+    height: 150px;
+    width: 100%;
+    h1 {
+      display: flex;
+      align-items: center;
+    }
+  }
 `;
 
-const TopBox = styled.section`
+const TitleBox = styled.section`
+  background-color: #ebf4fb;
+  border: 1px solid hsl(206, 85%, 57.5%);
+  border-radius: 2px;
   display: flex;
-  justify-content: center;
-  align-items: center;
   flex-direction: column;
-  width: 100%;
-  top: 60px;
+  height: max-content;
+  margin-top: 15px;
+  min-width: 400px;
   text-align: left;
-  h1 {
-    margin: 30px;
-  }
+  width: 80%;
   .pbox {
-    background-color: #ebf4fb;
-    padding: 50px;
-    border: 1px solid hsl(206, 85%, 57.5%);
+    padding: 24px;
     border-radius: 1px;
+    * {
+      margin-bottom: 5px;
+    }
+  }
+  h5 {
+    margin-top: 10px;
   }
 `;
-
-// const PBox = styled.div`
-//   display: flex;
-//   justify-content: start;
-//   align-items: center;
-//   flex-direction: column;
-//   text-align: left;
-// `;
 
 const PList = styled.div`
   text-align: left;
-  padding-left: 50px;
+  margin-top: 5px;
+  padding-left: 30px;
 `;
 
 const ContentBox = styled.section`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: column;
   width: 100%;
-  padding: 50px;
-  border: 3px solid black;
-  margin-top: 50px;
+  margin-top: 20px;
   .titlebox {
+    background-color: white;
     text-align: left;
-    padding: 50px;
+    padding: 30px;
+    border: 1px solid hsl(210, 8%, 90%);
+    border-radius: 2px;
+    display: flex;
+    flex-direction: column;
+    min-width: 400px;
+    width: 80%;
+    > input {
+      border: 1px solid hsl(210, 8%, 90%);
+      border-radius: 4px;
+      height: 30px;
+      margin-top: 10px;
+      width: 100%;
+      padding: 5px;
+    }
+  }
+  .title {
+    font-weight: 600;
+    font-size: 1.15rem;
   }
   .content {
     text-align: left;
     flex-direction: column;
-    padding: 50px;
+    padding: 24px;
+    background-color: white;
+    text-align: left;
+    padding: 30px;
+    border: 1px solid hsl(210, 8%, 90%);
+    border-radius: 2px;
+    display: flex;
+    flex-direction: column;
+    min-width: 400px;
+    width: 80%;
+    margin-top: 20px;
+    textarea {
+      max-height: calc(var(--s-step) * 6);
+    }
+  }
+  button {
+    color: white;
+    background-color: hsl(206, 100%, 52%);
+    margin-top: 20px;
   }
 `;
 
@@ -80,8 +126,10 @@ const CreateQuestion = () => {
     <CreateBox>
       <Header />
       <Content>
-        <h1>Ask a public question</h1>
-        <TopBox>
+        <div className="top">
+          <h1>Ask a public question</h1>
+        </div>
+        <TitleBox>
           <div className="pbox">
             <h2>Writing a good question</h2>
             <p>
@@ -104,7 +152,7 @@ const CreateQuestion = () => {
               <li>Review your question and post it to the site.</li>
             </PList>
           </div>
-        </TopBox>
+        </TitleBox>
         <ContentBox>
           <div className="titlebox">
             <div className="title">Title</div>
@@ -115,14 +163,14 @@ const CreateQuestion = () => {
             <input placeholder="e.g. Is there on R function for finding index of an element in a vertor?"></input>
           </div>
           <div className="content">
-            <div>What are the details of your problem?</div>
+            <div className="title">What are the details of your problem?</div>
             <p>
               Introduce the problem and expand on what you put in the title.
               Minimum 20 characters.
             </p>
             <textarea></textarea>
-            <button>Review your question</button>
           </div>
+          <button>Review your question</button>
         </ContentBox>
       </Content>
       <Footer />
@@ -133,9 +181,9 @@ const CreateQuestion = () => {
 export default CreateQuestion;
 
 /* TODO:
-1. 기본 구조 구현
+1. 기본 구조 구현 * 
 2. styled-components 적용
-3. input 창 구현
+3. input 창 구현 * 
 4. input value 구현
 5. input 창 클릭 시 옆에 modal 구현
 */
